045587409ea489d1077c449c4fb90e48c2745c60
Justin Wi <145292243+jvstin239@users.noreply.github.com>
2023-10-19 20:45:44 +0200

    Unterstriche und utf-8

--- main.py
+++ main.py
@@ -92,14 +92,20 @@ for wert in wkns:
 
     actions.send_keys(wert)
 
+    time.sleep(0.1)
+
     actions.send_keys(Keys.ENTER)
 
     actions.perform()
 
     time.sleep(0.5)
 
-    peergroup_button = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="grid"]/div[3]/div[1]/div[2]/div[2]/div[1]/div[3]')))
-    peergroup_button.click()
+    try:
+        peergroup_button = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="grid"]/div[3]/div[1]/div[2]/div[2]/div[1]/div[3]')))
+        peergroup_button.click()
+    except:
+        print("Peergroup Button konnte nicht geklickt werden bei: " + wert)
+        continue
     time.sleep(1)
     html = driver.page_source
     soup = BeautifulSoup(html, "html.parser")
@@ -112,26 +118,35 @@ for wert in wkns:
         time.sleep(0.2)
         new_html = driver.page_source
         new_soup = BeautifulSoup(new_html, "html.parser")
-        daten.append(new_soup.select(".accordion__parameter")[0].findAll("span")[0].text.strip())
-        daten.append(soup.findAll("th")[i].text.strip())
-        daten.append(new_soup.select(".accordion__parameter")[0].findAll("span")[1].text.strip())
-        daten.append(new_soup.select_one(".industry").text.strip())
-        daten.append(new_soup.select(".accordion__parameter")[6].select_one(".button").text.strip())
+        try:
+            daten.append(new_soup.select(".accordion__parameter")[0].findAll("span")[0].text.strip())
+            daten.append(soup.findAll("th")[i].text.strip())
+            daten.append(new_soup.select(".accordion__parameter")[0].findAll("span")[1].text.strip())
+            daten.append(new_soup.select_one(".industry").text.strip())
+            daten.append(new_soup.select(".accordion__parameter")[6].select_one(".button").text.strip())
+        except:
+            print("Element hat kein Attribut text bei " + wert + " in peergroup Position " + str(i))
+            continue
         # objekt = Aktie(soup.findAll("th")[i].text.strip(), wkn, isin, branche, sektor)
         driver.find_element(By.XPATH, '//*[@id="grid"]/div[4]/div[1]/div[1]/div[2]/i[6]').click()
         # print(soup.findAll("tr")[1].select_one("td").text)
 
         # Schleife über die Reihen in der Übersicht
         daten.append(soup.findAll("tr")[1].select("td")[i].select_one(".stock3Score__total").text.strip().replace("\u202f%", ""))
-        for row in soup.findAll("tr")[2:]:
-            daten.append(row.select("td")[i].text.strip().replace("\u202f%", ""))
+
+        try:
+            for row in soup.findAll("tr")[2:]:
+                daten.append(row.select("td")[i].text.strip().replace("\u202f%", ""))
+        except:
+            print("Fehler in Schleife über die einzelnen Reihen bei " + wert + " in Position " + str(i))
+            continue
 
         liste.append(daten)
-        columns = ["WKN", "Name", "ISIN", "Branche", "Sektor", "Stock3Score", "Momentum & Vola", "Kursperform 6 M", "Kursperform 1 J", "Delta 52 Wochen Hoch", "Vola 1 J",
-                   "Bewertung", "KUV", "Free Cash Flow", "PEG Ratio", "KGV (2023)", "KGV (2024)", "Kursziel", "Wachstum", "Umsatzwachstum ueber 5 J", "EPS-Wachstum ueber 5 J",
-                   "Umsatzwachstum (2023)", "Umsatzwachstum (2024)", "Wachstum des verwaesserten Gewinns je Aktie (2023)", "Wachstum des verwaesserten Gewinns je Aktie (2024)",
-                   "Qualitaet und Verschuldung", "Eigenkapitalrendite", "Eigenkapitalquote", "EBIT-Marge","Liquiditaet dritten Grades", "Liquiditaet zweiten Grades", "Verhaeltnis aus Schulden zum EK",
-                   "Verhaeltnis aus Schulden und  Vermoegenswerten", "Zinsdeckungsgrad", "Dividende & Aktienrueckkaeufe", "Gesamtrendite", "Payout Ratio", "Dividende (2023)", "Dividende (2024"]
+        columns = ["WKN", "Name", "ISIN", "Branche", "Sektor", "Stock3Score", "Momentum_&_Vola", "Kursperform 6 M", "Kursperform_1_J", "Delta_52_Wochen_Hoch", "Vola_1_J",
+                   "Bewertung", "KUV", "Free_Cash_Flow", "PEG_Ratio", "KGV_(2023)", "KGV_(2024)", "Kursziel", "Wachstum", "Umsatzwachstum_ueber_5_J", "EPS-Wachstum_ueber_5_J",
+                   "Umsatzwachstum_(2023)", "Umsatzwachstum_(2024)", "Wachstum_des_verwaesserten_Gewinns_je_Aktie_(2023)", "Wachstum_des_verwaesserten_Gewinns_je_Aktie_(2024)",
+                   "Qualitaet_und_Verschuldung", "Eigenkapitalrendite", "Eigenkapitalquote", "EBIT-Marge","Liquiditaet_dritten_Grades", "Liquiditaet_zweiten_Grades", "Verhaeltnis_aus_Schulden_zum_EK",
+                   "Verhaeltnis_aus_Schulden_und_Vermoegenswerten", "Zinsdeckungsgrad", "Dividende_&_Aktienrueckkaeufe", "Gesamtrendite", "Payout_Ratio", "Dividende_(2023)", "Dividende_(2024"]
 
     WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="grid"]/div[3]/div[1]/div[2]/div[2]/div/div[2]'))).click()
 
@@ -140,4 +155,4 @@ for wert in wkns:
 df = pandas.DataFrame(liste, columns = columns)
 folder = os.path.dirname(__file__)
 filename = "stock3_" + datetime.datetime.strftime(datetime.datetime.now(), "%d.%m.%y_%H%M") + ".csv"
-df.to_csv(os.path.join(folder, filename), sep=";", index = False)
+df.to_csv(os.path.join(folder, filename), sep=";", index = False, encoding = "utf-8")
