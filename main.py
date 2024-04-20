e59549705ca8012df669d32b8e18a658ddb2680c
jvstin239 <wild.justin@online.de>
2024-04-20 16:03:12 +0200

    Wartezeit verkürzt

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ driver.find_elements(By.CLASS_NAME, "widget__dropzone-button")[2].click()
 
 time.sleep(2)
 
-driver.find_element(By.XPATH, '//*[@id="grid"]/div[3]/div[1]/div[3]/div[2]/div[2]/div[2]/div[33]/div/div[2]/div[1]').click()
+driver.find_element(By.XPATH, '//*[@id="grid"]/div[3]/div[1]/div[3]/div[2]/div[2]/div[2]/div[32]/div/div[2]/div[1]').click()
 
 time.sleep(1)
 
@@ -112,7 +112,7 @@ for wert in wkns:
         time.sleep(0.5)
 
         try:
-            peergroup_button = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="grid"]/div[3]/div[1]/div[2]/div[2]/div[1]/div[3]')))
+            peergroup_button = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="grid"]/div[3]/div[1]/div[2]/div[2]/div[1]/div[3]')))
             peergroup_button.click()
         except:
             print("Peergroup Button konnte nicht geklickt werden bei: " + wert)
@@ -136,8 +136,8 @@ for wert in wkns:
                 wkn = new_soup.select(".accordion__parameter")[0].findAll("span")[0].text.strip()
                 if wkn in wkns_bereits_drin:
                     # driver.find_element(By.XPATH, '//*[@id="grid"]/div[4]/div[1]/div[1]/div[2]/i[6]').click()
-                    close_icon = WebDriverWait(driver, 10).until(
-                        EC.element_to_be_clickable((By.XPATH, '//*[@id="grid"]/div[4]/div[1]/div[1]/div[2]/div[2]/i[6]')))
+                    close_icon = WebDriverWait(driver, 1).until(
+                        EC.element_to_be_clickable((By.XPATH, '// *[ @ id = "grid"] / div[4] / div[1] / div[1] / div[2] / div / i[6]')))
                     close_icon.click()
                     break
                 wkns_bereits_drin.append(wkn)
